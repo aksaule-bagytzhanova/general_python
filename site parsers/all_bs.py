9daae76bc2bb7d4b0d731c1136dcfdf1ae6ac9e5
Aksaule <aksaule124026@gmail.com>
2021-10-04 22:25:57 +0600

    Lrn .find_next() .find_next_sibling() .find_previous() .find_previous_sibling()

--- site parsers/all_bs.py
+++ site parsers/all_bs.py
@@ -41,4 +41,76 @@ print(user_name)
 
 '''
 
-find_all_spans_in_user_info = soup.find(class_="user__info")
+'''
+find_all_spans_in_user_info = soup.find(class_="user__info").find_all("span")
+print(find_all_spans_in_user_info)
+
+for i in find_all_spans_in_user_info:
+    print(i.text)
+'''
+
+'''
+social_links = soup.find(class_="social__networks").find("ul").find_all("a")
+print(social_links)
+
+all_a = soup.find_all("a")
+print(all_a)
+
+for item in all_a:
+    item_text = item.text
+    item_url = item.get("href")
+    print(f"{item_text}: {item_url}")
+'''
+
+
+# .find_parent() .find_parents()
+
+'''
+post_div = soup.find(class_="post__text").find_parent()
+print(post_div)
+
+
+post_divs = soup.find(class_="post__text").find_parents("div", "user__post")
+print(post_divs)
+'''
+
+# .next_element  .previous_element
+'''
+
+next_el = soup.find(class_="post__title").next_element.next_element.text
+print(next_el)
+
+next_el = soup.find(class_="post__title").find_next().text
+print(next_el)
+
+'''
+
+# .find_next_sibling() .find_previous_sibling()
+'''
+next_sib = soup.find(class_="post__title").find_next_sibling()
+print(next_sib)
+
+prev_sib = soup.find(class_="post__date").find_previous_sibling()
+print(prev_sib)
+'''
+
+'''
+post_title = soup.find(class_="post__date").find_previous_sibling().find_next().text
+print(post_title)
+'''
+
+'''
+links = soup.find(class_="some__links").find_all("a")
+print(links)
+
+for link in links:
+    link_href_attr = link.get("href")
+    link_href_attr1 = link["href"]
+
+    link_data_attr = link.get("data-attr")
+    link_data_attr1 = link["data-attr"]
+
+    print(link_href_attr)
+    print(link_data_attr)
+'''
+
