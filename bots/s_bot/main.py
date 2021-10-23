7ce3abed00952e7eb083df2aa07796f741b08295
Aksaule <aksaule124026@gmail.com>
2021-10-23 20:34:22 +0600

    new files

--- bots/s_bot/main.py
+++ bots/s_bot/main.py
@@ -20,11 +20,40 @@ def welcome(message):
 
 
 
-    bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!\n - <b>, бот созданный чтобы быть подопытным кроликом.".format(message.from_user, bot.get_me()), parse_mode='html', reply_markup=markup)
+    bot.send_message(message.chat.id, "Добро пожаловать, !\nЯ бот созданный чтобы быть подопытным кроликом.".format(message.from_user, bot.get_me()), parse_mode='html', reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
-def lalala(message):
-    bot.send_message(message.chat.id, message.text)
 
+def lalala(message):
+    if message.chat.type == 'private':
+        if message.text == 'Рандомное число':
+            bot.send_message(message.chat.id, str(random.randint(0, 100)))
+        elif message.text == 'Как дела?':
+
+            markup = types.InlineKeyboardMarkup(row_width=2)
+            item1 = types.InlineKeyboardMarkup('Хорошо', callback_data='good')
+            item2 = types.InlineKeyboardMarkup('Не очень', callback_data='bad')
+
+            markup.add(item1, item2)
+
+            bot.send_message(message.chat.id, 'Отлично, сам как?', reply_markup=markup)
+        else:
+            bot.send_message(message.chat.id, 'Я не знаю что ответить')
+
+@bot.callback_query_handlers(func=lambda call: True)
+def callback_inline(call):
+    try:
+        if call.message:
+            if call.data == 'good':
+                bot.send_message(call.message.chat.id, 'Вот и отличненько ')
+            elif call.data == 'bad':
+                bot.send_message(call.message.chat.id, 'Бывает ')
+
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Как дела?", reply_markup=None)
+
+            bot.answer_callback_query(chat_id=call.message.chat.id, show_alert=False,
+                                      text="Это тестовое увеломление!!!1111")
+    except Exception as e:
+        print(repr(e))
 
 bot.polling(none_stop=True)
