3a097aa8264695f9a55499f4de5023be066c8ddf
Aksaule <aksaule124026@gmail.com>
2021-11-11 17:25:43 +0600

    Last changes with django

--- django/mysite/polls/admin.py
+++ django/mysite/polls/admin.py
@@ -4,3 +4,15 @@ from .models import Question, Choice
 
 admin.site.register(Question)
 admin.site.register(Choice)
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
+
+
--- django/mysite/polls/tests.py
+++ django/mysite/polls/tests.py
@@ -83,4 +83,3 @@ class QuestionDetailViewTests(TestCase):
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
 
-        
