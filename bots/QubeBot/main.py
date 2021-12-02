4e9d749bac39990949f3ae1833b0e4292b5a0a6a
Aksaule <aksaule124026@gmail.com>
2021-12-02 16:11:42 +0600

    Start test task

--- bots/QubeBot/main.py
+++ bots/QubeBot/main.py
@@ -1,6 +1,12 @@
+from telebot import types
 import config
 import telebot
+import time as tm
 
+spams = {}
+msgs = 4
+max = 5
+ban = 300
 bot = telebot.TeleBot(config.TOKEN)
 black_list_text = ['join', 'https://t.me/joinchat/', 'click on', 'join', 'follow', 'https://t.me/', 'https://nft-punks.net/']
 while_list_text = ['https://theqube.cc', 'https://github.com/github-qubecs/documents/blob/main/qube-whitepaper-1.0.pdf',
@@ -31,8 +37,34 @@ def handler_new_member(message):
     user_name = message.new_chat_member.first_name
     user_surn = message.new_chat_member.last_name
     bot.send_message(message.chat.id, "Добро пожаловать, {0} {1}!".format(user_name, user_surn))
+    markup_inline = types.Telebot
     #bot.send_message(message.from_user.id, "Добро пожаловать, {0}!".format(user_name))
     #bot.send_message(message.chat.id, op)
+
+
+def is_spam(user_id):
+    try:
+        usr = spams[user_id]
+        usr["messages"] += 1
+    except:
+        spams[user_id] = {"next_time": int(tm.time()) + max, "messages": 1, "banned": 0}
+        usr = spams[user_id]
+    if usr["banned"] >= int(tm.time()):
+        return True
+    else:
+        if usr["next_time"] >= int(tm.time()):
+            if usr["messages"] >= msgs:
+                spams[user_id]["banned"] = tm.time() + ban
+                # text = """You're banned for {} minutes""".format(ban/60)
+                # bot.send_message(user_id, text)
+                # User is banned! alert him...
+                return True
+        else:
+            spams[user_id]["messages"] = 1
+            spams[user_id]["next_time"] = int(tm.time()) + max
+    return False
+
+
 
 
 
