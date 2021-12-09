b81dfd75cac560801cd0fdfbaa60d858f0d61b8f
Aksaule <aksaule124026@gmail.com>
2021-12-09 12:52:53 +0600

    OpenCV lesson5

---
+++ OpenCV/lesson5.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+cv2.namedWindow("frame")
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    framex = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=5)
+    framey = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=5)
+    edge = cv2.Canny(gray, 100, 200)
+
+
+    cv2.imshow("frame", frame)
+    cv2.imshow("x", framex)
+    cv2.imshow("y", framey)
+    cv2.imshow("edge",edge)
+    k = cv2.waitKey(1) & 0xFF
+    if k==27:
+        break
+
+cv2.destroyAllWindows()
